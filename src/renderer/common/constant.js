54fe5dd69bbbe8d38aa3311a121f43d0549ffa4e
hanxinlou <1252543725@qq.com>
2020-05-06 00:18:48 +0800

    style: 添加毕设标题和说明图像或视频的标签

--- src/renderer/common/constant.js
+++ src/renderer/common/constant.js
@@ -1,8 +1,8 @@
 // 菜单列表
 export const menuList = [
   {
-    title: '畸变研究',
-    icon: 'el-icon-search',
+    title: '图像处理',
+    icon: 'el-icon-picture-outline',
     path: '/distortion'
   },
   {
